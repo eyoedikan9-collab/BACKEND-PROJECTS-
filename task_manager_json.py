64af87eb7743259ba60bc6ca622c55ad7519374d
enyeneraph <enyenerudolf@gmail.com>
2026-02-26 12:42:25 +0100

    Enhance task management functionality: add status handling, improve task viewing, and refine task deletion

--- task_manager_json.py
+++ task_manager_json.py
@@ -1,9 +1,10 @@
 import json
+
 def add_task():
     task_ = input("Enter task: ")
     date_time = input("Enter task time: ")
     duration = input("Enter task duration: ")
-    new_task_data = {"Task": task_, "Date/Time": date_time, "Duration": duration}
+    new_task_data = {"Task": task_, "Date/Time": date_time, "Duration": duration, "status": "pending"}
     try:
         with open('task.json', 'r') as f:
             data = json.load(f)
@@ -20,59 +21,44 @@ def view_task():
     try:
         with open('task.json', 'r') as f:
             data = json.load(f)
-            # if isinstance(data, dict):
-            #     data = [data]
-            # list = []
-            # list.append(data)
-            # for task in data:
-            print(data)
+    
+        for dict_ in data:
+            for key, value in dict_.items():
+                print(f"{key}: {value}")
+            print("=============")
 
     except FileNotFoundError:
-        print("Nothing Found in json")
+        print("File does not exist.")
+
+
 def mark_task():
+    view_task()
     task_name = input("What is the name of the task you want to mark as complete? ")
     print(task_name)
-    status_ = input("Status: ")
-    print(status_)
-    key = "Status"
-    set_status = ["Completed", "Pending"]
-    if status_ not in set_status:
-                print("Invalid status choice, please check and try again")
-                task_menu()
-    else:
-        with open('task.json', 'r') as f:
-            data = json.load(f)
-            part_task = data['Task'] == task_name
-            if part_task:
-                data[key] = status_
-                
-    #     for new_task_data in data:
-    #                 getting = new_task_data.get("Task")
-    # if task_name in getting:
-    #     status_ = input("Status: ")
-            
-    # set_status = ["Completed", "Pending"]
-    # if status_ not in set_status:
-    #     print("Invalid status choice, please check and try again")
-    # else:
-           
-    #     print(f"Status :{status_} is being added to {getting} task")
+    with open('task.json', 'r') as f:
+        data = json.load(f)
+
+    for task_details in data:
+        if task_details['Task'] == task_name:
+            task_details['status'] = 'completed'
+
+    with open('task.json', 'w') as f:
+        json.dump(data, f, indent=2)
+    
+
 def delete_task():
+    view_task()
     del_task = input("Enter name of task to be deleted: ")
     print(del_task)
     with open('task.json', 'r') as f:
         data = json.load(f)
         for task in data:
-            if data["Task"] == del_task:
-                pass
-        
+            if task["Task"] == del_task:
+                data.remove(task) 
+                print(data)
+    
 
-      
 
-             
-
-# def exit_task():
-#     print('All task save bye')
 def task_menu():
     print("""
     1. Add Task
@@ -85,19 +71,27 @@ def task_menu():
 
     if user_choice in range(1, 6):
         if user_choice == 1:
+            print("Adding task")
             add_task()
         elif user_choice == 2:
+            print("Viewing task")
             view_task() 
         elif user_choice == 3:
+            print("Marking task")
             mark_task() 
         elif user_choice == 4:
+            print("Deleting task")
             delete_task()
         elif user_choice == 5:
-            return "Exit"         
+            print("Exiting program")
+            return "Exit"   
     else:
         print("Invalid choice please check and try again")
-        task_menu()
+        # task_menu()
+
+
 while True:
-    tk_m = task_menu()
-    if tk_m == 5
+    output = task_menu()
+    if output == "Exit":
+        break
  
